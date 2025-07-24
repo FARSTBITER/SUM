704bf42ab2e60b76abec158652445fa372708456
FARSTBITER <207075785+FARSTBITER@users.noreply.github.com>
2025-07-24 07:34:48 +0300

    Added fuel registry

--- src/main/java/com/sum/item/SUMItems.java
+++ src/main/java/com/sum/item/SUMItems.java
@@ -1,5 +1,6 @@
 package com.sum.item;
 
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -14,6 +15,16 @@ public class SUMItems {
 	private static void registerItem(Item item, RegistryKey<Item> registryKey) {
 		
 		Registry.register(Registries.ITEM, registryKey, item);
+		
+	}
+	
+	private static void registerFuel(Item item, int burnTime) {
+		
+		FuelRegistryEvents.BUILD.register((builder, context) -> {
+			
+			builder.add(item, burnTime);
+			
+		});
 		
 	}
 
