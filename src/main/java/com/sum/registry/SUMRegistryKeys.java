c0376c8e2be5e052b642eb8dc39afaa8fc81b3cc
FARSTBITER <207075785+FARSTBITER@users.noreply.github.com>
2025-07-24 07:51:22 +0300

    Added own item group

--- src/main/java/com/sum/registry/SUMRegistryKeys.java
+++ src/main/java/com/sum/registry/SUMRegistryKeys.java
@@ -1,5 +1,12 @@
 package com.sum.registry;
 
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+
 public class SUMRegistryKeys {
+	
+	public static final RegistryKey<ItemGroup> SUM_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of("sum", "item_group"));
 
 }
