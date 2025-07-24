ac97d63bceedd79d6f4d3d491dd1a61b166e17ff
FARSTBITER <207075785+FARSTBITER@users.noreply.github.com>
2025-07-24 07:42:20 +0300

    Added item group registry method

--- src/main/java/com/sum/item/SUMItemGroups.java
+++ src/main/java/com/sum/item/SUMItemGroups.java
@@ -1,8 +1,19 @@
 package com.sum.item;
 
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+
 public class SUMItemGroups {
 	
 	public static void registerItemGroups() {
+		
+	}
+	
+	private static void registerItemGroup(ItemGroup itemGroup, RegistryKey<ItemGroup> registryKey) {
+		
+		Registry.register(Registries.ITEM_GROUP, registryKey, itemGroup);
 		
 	}
 
