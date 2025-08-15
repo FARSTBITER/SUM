90f97c2ef3b138479cfc40a945bb1a807c416c3a
FARSTBITER <207075785+FARSTBITER@users.noreply.github.com>
2025-08-15 23:21:14 +0300

    Added brown coal to some villager trades

--- src/main/java/com/sum/village/SUMTradeOffers.java
+++ src/main/java/com/sum/village/SUMTradeOffers.java
@@ -1,8 +1,11 @@
 package com.sum.village;
 
+import com.sum.item.SUMItems;
+
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradedItem;
@@ -11,6 +14,12 @@ import net.minecraft.village.VillagerProfession;
 public class SUMTradeOffers {
 	
 	public static void initialize() {
+		
+		registerTradeOffer(VillagerProfession.ARMORER, 1, SUMItems.BROWN_COAL, 18, Items.EMERALD, 1, 20, 2, 0.05F);
+		registerTradeOffer(VillagerProfession.BUTCHER, 2, SUMItems.BROWN_COAL, 18, Items.EMERALD, 1, 20, 2, 0.05F);
+		registerTradeOffer(VillagerProfession.FISHERMAN, 1, SUMItems.BROWN_COAL, 12, Items.EMERALD, 1, 20, 2, 0.05F);
+		registerTradeOffer(VillagerProfession.TOOLSMITH, 1, SUMItems.BROWN_COAL, 18, Items.EMERALD, 1, 20, 2, 0.05F);
+		registerTradeOffer(VillagerProfession.WEAPONSMITH, 1, SUMItems.BROWN_COAL, 18, Items.EMERALD, 1, 20, 2, 0.05F);
 		
 	}
 	
